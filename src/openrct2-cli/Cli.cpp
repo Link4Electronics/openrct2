a2713f4158c786dbfe58a75123754dfb4f9b5da5
Link4Electronics <179417190+Link4Electronics@users.noreply.github.com>
2026-08-12 01:19:13 -0300

    Initial commit

---
+++ src/openrct2-cli/Cli.cpp
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * Copyright (c) 2014-2026 OpenRCT2 developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
+ *
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#include <openrct2/Context.h>
+#include <openrct2/OpenRCT2.h>
+#include <openrct2/command_line/CommandLine.hpp>
+
+using namespace OpenRCT2;
+using namespace OpenRCT2::CommandLine;
+
+/**
+ * Main entry point for non-Windows systems. Windows instead uses its own DLL proxy.
+ */
+int main(int argc, const char** argv)
+{
+    int32_t rc = EXIT_SUCCESS;
+    auto runGame = CommandLineRun(argv, argc);
+    if (runGame == ExitCode::launch)
+    {
+        gOpenRCT2Headless = true;
+        gOpenRCT2NoGraphics = true;
+
+        // Run OpenRCT2 with a plain context
+        auto context = CreateContext();
+        rc = context->RunOpenRCT2(argc, argv);
+    }
+    else if (runGame == ExitCode::fail)
+    {
+        rc = EXIT_FAILURE;
+    }
+    return rc;
+}
---
+++ src/openrct2/command_line/SimulateCommands.cpp
@@ -0,0 +1,88 @@
+/*****************************************************************************
+ * Copyright (c) 2014-2026 OpenRCT2 developers
+ *
+ * For a complete list of all authors, please refer to contributors.md
+ * Interested in contributing? Visit https://github.com/OpenRCT2/OpenRCT2
+ *
+ * OpenRCT2 is licensed under the GNU General Public License version 3.
+ *****************************************************************************/
+
+#include "../Context.h"
+#include "../Game.h"
+#include "../GameState.h"
+#include "../OpenRCT2.h"
+#include "../config/ConfigTypes.h"
+#include "../core/Console.hpp"
+#include "../entity/EntityRegistry.h"
+#include "../network/Network.h"
+#include "../platform/Platform.h"
+#include "CommandLine.hpp"
+
+#include <cstdlib>
+#include <memory>
+
+using namespace OpenRCT2::CommandLine;
+
+namespace OpenRCT2
+{
+    // clang-format off
+    static constexpr CommandLineOptionDefinition kNoOptions[]
+    {
+        kOptionTableEnd
+    };
+
+    static ExitCode HandleSimulate(CommandLineArgEnumerator* argEnumerator);
+
+    const CommandLineCommand CommandLine::kSimulateCommands[]{
+        // Main commands
+        DefineCommand("", "<park file> <ticks>", kNoOptions, HandleSimulate),
+        kCommandTableEnd
+    };
+    // clang-format on
+
+    static ExitCode HandleSimulate(CommandLineArgEnumerator* argEnumerator)
+    {
+        const utf8* inputPath;
+        if (!argEnumerator->TryPopString(&inputPath))
+        {
+            Console::Error::WriteLine("Expected a save file path");
+            return ExitCode::fail;
+        }
+
+        int32_t ticks;
+        if (!argEnumerator->TryPopInteger(&ticks))
+        {
+            Console::Error::WriteLine("Expected a number of ticks to simulate");
+            return ExitCode::fail;
+        }
+
+        gOpenRCT2Headless = true;
+
+#ifndef DISABLE_NETWORK
+        gNetworkStart = Network::Mode::server;
+#endif
+
+        std::unique_ptr<IContext> context(CreateContext());
+        if (context->Initialise())
+        {
+            if (!context->LoadParkFromFile(inputPath))
+            {
+                return ExitCode::fail;
+            }
+
+            Console::WriteLine("Running %d ticks...", ticks);
+            for (int32_t i = 0; i < ticks; i++)
+            {
+                gameStateUpdateLogic();
+            }
+            Console::WriteLine("Completed: %s", getGameState().entities.GetAllEntitiesChecksum().ToString().c_str());
+        }
+        else
+        {
+            Console::Error::WriteLine("Context initialization failed.");
+            return ExitCode::fail;
+        }
+
+        return ExitCode::ok;
+    }
+} // namespace OpenRCT2
